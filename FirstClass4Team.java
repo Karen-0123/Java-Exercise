ba715759b7c7673c23218d5536161fb3cdb5a187
sereneee333 <01257172@email.ntou.edu.tw>
2025-04-28 00:02:49 +0800

    update_01257172鄭寧
    印出("\"學號：\"01257116\"\n姓名：\"康慈軒\"");

--- FirstClass4Team.java
+++ FirstClass4Team.java
@@ -3,5 +3,6 @@ public class FirstClass4Team {
         System.out.println("這是用來練習Github協作的程式!");
         System.out.println("\"學號：\"01257154\"\n姓名：\"李宛芸\"");
         System.out.println("\"學號：\"01257116\"\n姓名：\"康慈軒\"");
+        System.out.println("\"學號：\"01257172\"\n姓名：\"鄭寧\"");
     }
 }
